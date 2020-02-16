4cad4c33d2b8bd42499f4324e8cda270d6092f5c
seabbs <s.e.abbott12@gmail.com>
2020-02-16 21:50:55 +0000

    finalised generation size plot

--- R/plot_generation_size.R
+++ R/plot_generation_size.R
@@ -11,22 +11,24 @@
 #' 
 #' 
 plot_generation_size <- function(generation_sizes) {
-  plot <-  ggplot2::ggplot(generation_sizes, 
+  plot <- ggplot2::ggplot(generation_sizes, 
                            ggplot2::aes(x = gen, y = median, 
                                         col = control, fill = control)) +
     ggplot2::geom_ribbon(ggplot2::aes(ymin = bottom, ymax = top, col = NULL), 
                          alpha = 0.1, show.legend = FALSE) +
     ggplot2::geom_ribbon(ggplot2::aes(ymin = lower, ymax = upper, col = NULL), 
                          alpha = 0.3, show.legend = FALSE) +
-    ggplot2::geom_point(alpha = 0.4, show.legend = FALSE) +
-    ggplot2::geom_line(alpha = 1, size = 1.1, show.legend = FALSE) +
+    ggplot2::geom_point(aes(size = sims), alpha = 0.4) +
+    ggplot2::geom_line(alpha = 1, show.legend = FALSE) +
     cowplot::theme_cowplot() +
     ggplot2::labs(
       x = "Outbreak generation",
       y = "Cases in generation"
     ) +
     ggplot2::theme(legend.position = "bottom") +
-    ggplot2::scale_colour_brewer(palette = "Set1", direction = -1) +
-    ggplot2::scale_fill_brewer(palette = "Set1", direction = -1)
+    ggplot2::scale_size_continuous(breaks = seq(2, max(generation_sizes$sims), 2)) +
+    ggplot2::scale_colour_brewer(palette = "Set1", direction = -1, guide = FALSE) +
+    ggplot2::scale_fill_brewer(palette = "Set1", direction = -1, guide = FALSE) +
+    ggplot2::guides(size = ggplot2::guide_legend(title = "Simulations that reach this generation"))
   return(plot)
 }
